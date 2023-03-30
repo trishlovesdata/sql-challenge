ee4f2b8b6e0a2355bd5dc73719937f3b48a177d5
trishlovesdata <114893984+trishlovesdata@users.noreply.github.com>
2023-03-29 20:19:25 -0700

    updating files

---
+++ Schema.sql
@@ -0,0 +1,75 @@
+﻿
+
+CREATE TABLE "dept_manager" (
+    "dept_no" varchar   NOT NULL,
+    "emp_no" int   NOT NULL,
+    PRIMARY KEY (
+        "dept_no","emp_no"
+     )
+);
+
+CREATE TABLE "departments" (
+    "dept_no" varchar   NOT NULL,
+    "dept_name" varchar   NOT NULL,
+    PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" int   NOT NULL,
+    "dept_no" varchar   NOT NULL,
+    PRIMARY KEY (
+        "emp_no","dept_no"
+     )
+);
+
+CREATE TABLE "employees" (
+    "emp_no" int   NOT NULL,
+    "emp_title_id" varchar   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "first_name" varchar   NOT NULL,
+    "last_name" varchar   NOT NULL,
+    "sex" varchar(1)   NOT NULL,
+    "hire_date" date   NOT NULL,
+   PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+-- Table documentation comment 1 (try the PDF/RTF export)
+-- Table documentation comment 2
+CREATE TABLE "Salaries" (
+    "emp_no" int   NOT NULL,
+    "salary" int   NOT NULL,
+    PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "Titles" (
+    "title_id" varchar   NOT NULL,
+    "title" varchar   NOT NULL,
+     PRIMARY KEY (
+        "title_id"
+     )
+);
+
+ALTER TABLE "dept_manager"  ADD FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager"  ADD FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp"  ADD FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_emp"  ADD FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "Salaries" ADD FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "employees" ADD FOREIGN KEY("emp_title_id")
+REFERENCES "Titles" ("title_id");
+
